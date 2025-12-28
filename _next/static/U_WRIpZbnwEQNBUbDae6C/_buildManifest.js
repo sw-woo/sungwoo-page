717114d2a94e818f158c43be504a87d016459441
sw-woo <sw-woo@users.noreply.github.com>
2025-12-28 08:23:48 +0000

    deploy: cab720ea39841061bba15adb5bf95c2436105a16

---
+++ _next/static/U_WRIpZbnwEQNBUbDae6C/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sungwoo-page//_next/:path+",
+        "destination": "/sungwoo-page/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
